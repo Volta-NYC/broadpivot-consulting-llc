93087c020bb7c2ddbc8793901b83c40aa9b132aa
Andrew Chin <andrewchin530@gmail.com>
2026-07-14 02:05:50 -0400

    fix: raise hero headshot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,7 @@ export default function Home() {
             </div>
 
             {/* RIGHT: Melodye's headshot */}
-            <div className="hidden lg:flex justify-center lg:justify-end items-end lg:-translate-y-32">
+            <div className="hidden lg:flex justify-center lg:justify-end items-end lg:-translate-y-40">
               <div className="photo-frame relative" style={{ maxWidth: 400 }}>
                 <Image
                   src="/images/melodye-harvey.png"
